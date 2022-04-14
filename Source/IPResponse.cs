f776a5340bb818c365aec4717b84492c211dca1f
Banovvv <ivan_banov@abv.bg>
2022-04-14 20:22:10 +0300

    Added basic APResponse class structure

---
+++ Source/IPResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPGeo
+{
+	internal class IPResponse
+	{
+		public IPResponse()
+		{
+
+		}
+
+		public string Status { get; }
+		public string Country { get; }
+		public string CountryCode { get; }
+		public string Region { get; }
+		public string City { get; }
+		public string Zip { get; }
+		public double Latitude { get; }
+		public double Longitude { get; }
+		public string Timezone { get; }
+		public string Isp { get; }
+		public string Org { get; }
+		public string As { get; }
+		public string Query { get; }
+	}
+}
