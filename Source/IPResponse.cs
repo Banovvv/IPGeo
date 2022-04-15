fdd40af0f1e17ade8780f7c63adc72f419af94ec
Banovvv <ivan_banov@abv.bg>
2022-04-15 08:47:14 +0300

    Updated IPResponse class with all possible properties

--- Source/IPResponse.cs
+++ Source/IPResponse.cs
@@ -14,17 +14,29 @@ namespace IPGeo
 		}
 
 		public string Status { get; }
+		public string Message { get; }
+		public string Continent { get; }
+		public string ContinentCode { get; }
 		public string Country { get; }
 		public string CountryCode { get; }
 		public string Region { get; }
+		public string RegionName { get; }
 		public string City { get; }
+		public string District { get; }
 		public string Zip { get; }
 		public double Latitude { get; }
 		public double Longitude { get; }
 		public string Timezone { get; }
-		public string Isp { get; }
-		public string Org { get; }
+		public string TimezoneOffset { get; }
+		public string Currency { get; }
+		public string ISP { get; }
+		public string OrganizationName { get; }
 		public string As { get; }
-		public string Query { get; }
+		public string AsName { get; }
+		public string Reverse { get; }
+		public bool Mobile { get; }
+		public bool Proxy { get; }
+		public bool Hosting { get; }
+		public string IP { get; }
 	}
 }
