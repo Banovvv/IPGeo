0cf39b226e330eac07b2c0dcdf7555e0a805cea6
Banovvv <ivan_banov@abv.bg>
2022-04-18 13:19:37 +0300

    Added property descriptions

--- Source/IPResponse.cs
+++ Source/IPResponse.cs
@@ -42,30 +42,105 @@ namespace IPGeo
             }
         }
 
+        /// <summary>
+        /// Status of the request - success or fail
+        /// </summary>
         public string Status { get; }
+        /// <summary>
+        /// Included only when status is fail
+        /// </summary>
         public string Message { get; }
+        /// <summary>
+        /// Continent name
+        /// </summary>
         public string Continent { get; }
+        /// <summary>
+        /// Two-letter continent code	
+        /// </summary>
         public string ContinentCode { get; }
+        /// <summary>
+        /// Country name
+        /// </summary>
         public string Country { get; }
+        /// <summary>
+        /// Two-letter country code
+        /// </summary>
         public string CountryCode { get; }
+        /// <summary>
+        /// Region/state short code
+        /// </summary>
         public string Region { get; }
+        /// <summary>
+        /// Region/state
+        /// </summary>
         public string RegionName { get; }
+        /// <summary>
+        /// City
+        /// </summary>
         public string City { get; }
+        /// <summary>
+        /// District (subdivision of city)
+        /// </summary>
         public string District { get; }
+        /// <summary>
+        /// Zip code
+        /// </summary>
         public string Zip { get; }
+        /// <summary>
+        /// Latitude
+        /// </summary>
         public double Latitude { get; }
+        /// <summary>
+        /// Longitude
+        /// </summary>
         public double Longitude { get; }
+        /// <summary>
+        /// Timezone
+        /// </summary>
         public string Timezone { get; }
+        /// <summary>
+        /// Timezone UTC DST offset in seconds
+        /// </summary>
         public int TimezoneOffset { get; }
+        /// <summary>
+        /// National currency
+        /// </summary>
         public string Currency { get; }
+        /// <summary>
+        /// ISP name
+        /// </summary>
         public string ISP { get; }
+        /// <summary>
+        /// Organization name
+        /// </summary>
         public string OrganizationName { get; }
+        /// <summary>
+        /// AS number and organization, separated by space (RIR). Empty for IP blocks not being announced in BGP tables.
+        /// </summary>
         public string As { get; }
+        /// <summary>
+        /// AS name (RIR). Empty for IP blocks not being announced in BGP tables.	
+        /// </summary>
         public string AsName { get; }
+        /// <summary>
+        /// Reverse DNS of the IP
+        /// </summary>
         public string Reverse { get; }
+        /// <summary>
+        /// Mobile (cellular) connection
+        /// </summary>
         public bool Mobile { get; }
+        /// <summary>
+        /// Proxy, VPN or Tor exit address
+        /// </summary>
         public bool Proxy { get; }
+        /// <summary>
+        /// Hosting, colocated or data center
+        /// </summary>
         public bool Hosting { get; }
+        /// <summary>
+        /// IP used for the query
+        /// </summary>
         public string IP { get; }
     }
 }
